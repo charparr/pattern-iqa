73c100945fcd703aca2fedbab31453a9fc77d660
Charlie Parr <charlie@arcticfiredevelopment.com>
2017-09-25 16:54:59 -0800

    top5 now actually includes 5 features

--- util/process_sim_and_df_results.py
+++ util/process_sim_and_df_results.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.ndimage.interpolation import zoom
 from util.normalize import make_normalized_array
 from similarity_tests.similarity_metrics import compute_similarity
 
@@ -9,7 +10,8 @@ def perform_similarity(normed_depth, test_surfaces, ids):
 
     for surfs in zip(test_surfaces, ids):
         print("Processing... " + surfs[1])
-        results = compute_similarity(normed_depth, make_normalized_array(surfs[0]), surfs[1])
+        results = compute_similarity(normed_depth,
+                                     make_normalized_array(surfs[0]), surfs[1])
         rows_list.append(results)
 
     df = pd.DataFrame(rows_list)
@@ -21,6 +23,6 @@ def perform_similarity(normed_depth, test_surfaces, ids):
     df['FSIM Rank'] = df['fsim_value'].rank(ascending=False)
     df['Avg. Rank'] = (df['MSE Rank'] + df['SSIM Rank'] + df['CW-SSIM Rank'] + df['GMSD Rank'] + df['FSIM Rank']) / 5
     df = df.sort_values(['Avg. Rank'])
-    top5 = df.head(5)
+    top5 = df.head(6)
 
     return df, top5
